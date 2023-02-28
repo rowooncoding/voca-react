09b3767a9e04896fcd7a36c032b85957078f229c
Rowoon Lee <dev.rowoon@gmail.com>
2023-02-28 16:05:16 +0900

    [추가] 마크업 추가

--- src/component/Day.js
+++ src/component/Day.js
@@ -10,8 +10,15 @@ export default function Day() {
         <tbody>
           {wordList.map((word) => (
             <tr key={word.id}>
+              <td>
+                <input type="checkbox" />
+              </td>
               <td>{word.eng}</td>
               <td>{word.kor}</td>
+              <td>
+                <button>뜻 보기</button>
+                <button>삭제</button>
+              </td>
             </tr>
           ))}
         </tbody>
