706af69e0e4efce9a6f64818b051c758c765526a
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-01 13:25:15 +0900

    [추가] fetch 통신 부분 추가

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,15 +1,25 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import dummy from "../database/data.json";
 import Word from "./Word";
 export default function Day() {
   const { day } = useParams();
-  const wordList = dummy.words.filter((word) => word.day === +day);
+  // const wordList = dummy.words.filter((word) => word.day === +day);
+  const [words, setWords] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setWords(data);
+      });
+  }, [day]);
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map((word) => (
+          {words.map((word) => (
             <Word word={word} key={word.id} />
           ))}
         </tbody>
