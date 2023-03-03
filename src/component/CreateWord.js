4d53d94d45b58d013b0980a10378414278fadab7
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-03 13:49:36 +0900

    [추가] post를 사용한 단어 추가

--- src/component/CreateWord.js
+++ src/component/CreateWord.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import useFetch from "../hooks/useFetch";
 
 export default function CreateWord() {
@@ -6,19 +7,41 @@ export default function CreateWord() {
   function onSubmit(e) {
     e.preventDefault();
   }
+
+  const engRef = useRef(null);
+  const korRef = useRef(null);
+  const dayRef = useRef(null);
+
+  fetch(`http://localhost:3001/words/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      day: dayRef.current.value,
+      eng: engRef.current.value,
+      kor: korRef.current.value,
+      isDone: false,
+    }),
+  }).then((res) => {
+    if (res.ok) {
+      alert("생성이 완료되었습니다.");
+    }
+  });
+
   return (
     <form onSubmit={onSubmit}>
       <div className="input_area">
         <label>Eng</label>
-        <input type="text" placeholder="computer" />
+        <input type="text" placeholder="computer" ref={engRef} />
       </div>
       <div className="input_area">
         <label>Kor</label>
-        <input type="text" placeholder="컴퓨터" />
+        <input type="text" placeholder="컴퓨터" ref={korRef} />
       </div>
       <div className="input_area">
         <label>Day</label>
-        <select>
+        <select ref={dayRef}>
           {days.map((day) => (
             <option key={day.id} value={day.day}>
               {day.day}
