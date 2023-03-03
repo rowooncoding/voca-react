e2628fb6a5f5e254073acd7c8bf36d597c47978f
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-03 14:25:47 +0900

    [수정] 코드 위치 수정

--- src/component/CreateWord.js
+++ src/component/CreateWord.js
@@ -8,28 +8,27 @@ export default function CreateWord() {
 
   function onSubmit(e) {
     e.preventDefault();
+    fetch(`http://localhost:3001/words/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        day: dayRef.current.value,
+        eng: engRef.current.value,
+        kor: korRef.current.value,
+        isDone: false,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        alert("생성이 완료되었습니다.");
+        history(`/day/${dayRef.current.value}`);
+      }
+    });
   }
   const engRef = useRef(null);
   const korRef = useRef(null);
   const dayRef = useRef(null);
-
-  fetch(`http://localhost:3001/words/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      day: dayRef.current.value,
-      eng: engRef.current.value,
-      kor: korRef.current.value,
-      isDone: false,
-    }),
-  }).then((res) => {
-    if (res.ok) {
-      alert("생성이 완료되었습니다.");
-      history(`/day/${dayRef.current.value}`);
-    }
-  });
 
   return (
     <form onSubmit={onSubmit}>
